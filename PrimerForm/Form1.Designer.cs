17f2fef19e89533abaa10c95f4bc67d9508de89d
Ana Galvez <anamariagalvez01@gmail.com>
2025-03-26 01:49:33 -0300

    feat: practica1

--- PrimerForm/Form1.Designer.cs
+++ PrimerForm/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             lblNombre = new Label();
             txtNombre = new TextBox();
+            btnCuadro = new Button();
+            btnTexto = new Button();
+            btnDesdeTxtACuadro = new Button();
             SuspendLayout();
             // 
             // lblNombre
@@ -50,12 +53,45 @@
             txtNombre.Size = new Size(266, 20);
             txtNombre.TabIndex = 1;
             // 
+            // btnCuadro
+            // 
+            btnCuadro.Location = new Point(462, 219);
+            btnCuadro.Name = "btnCuadro";
+            btnCuadro.Size = new Size(221, 63);
+            btnCuadro.TabIndex = 2;
+            btnCuadro.Text = "Cuadro de Diálogo";
+            btnCuadro.UseVisualStyleBackColor = true;
+            btnCuadro.Click += btnCuadro_Click;
+            // 
+            // btnTexto
+            // 
+            btnTexto.Location = new Point(932, 198);
+            btnTexto.Name = "btnTexto";
+            btnTexto.Size = new Size(195, 62);
+            btnTexto.TabIndex = 3;
+            btnTexto.Text = "Cuadro Con Texto";
+            btnTexto.UseVisualStyleBackColor = true;
+            btnTexto.Click += btnTexto_Click;
+            // 
+            // btnDesdeTxtACuadro
+            // 
+            btnDesdeTxtACuadro.Location = new Point(716, 423);
+            btnDesdeTxtACuadro.Name = "btnDesdeTxtACuadro";
+            btnDesdeTxtACuadro.Size = new Size(322, 62);
+            btnDesdeTxtACuadro.TabIndex = 4;
+            btnDesdeTxtACuadro.Text = "Desde txt a cuadro";
+            btnDesdeTxtACuadro.UseVisualStyleBackColor = true;
+            btnDesdeTxtACuadro.Click += btnDesdeTxtACuadro_Click;
+            // 
             // frmPrimerProyecto
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.ForestGreen;
             ClientSize = new Size(1414, 609);
+            Controls.Add(btnDesdeTxtACuadro);
+            Controls.Add(btnTexto);
+            Controls.Add(btnCuadro);
             Controls.Add(txtNombre);
             Controls.Add(lblNombre);
             Name = "frmPrimerProyecto";
@@ -69,5 +105,8 @@
 
         private Label lblNombre;
         private TextBox txtNombre;
+        private Button btnCuadro;
+        private Button btnTexto;
+        private Button btnDesdeTxtACuadro;
     }
 }
--- PrimerForm/Form1.cs
+++ PrimerForm/Form1.cs
@@ -11,5 +11,23 @@ namespace PrimerForm
         {
 
         }
+
+        private void btnCuadro_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("¿Seguro que deseas salir del programa?", "ATENCIÓN", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+        }
+
+        private void btnTexto_Click(object sender, EventArgs e)
+        {
+            string texto;
+            texto = Microsoft.VisualBasic.Interaction.InputBox("Ingresa tu nombre por favor", "Bienvenido", "Ingresa aquí tu nombre");
+            txtNombre.Text = texto;
+        }
+
+        private void btnDesdeTxtACuadro_Click(object sender, EventArgs e)
+        {
+            string nombre=txtNombre.Text;
+            MessageBox.Show(nombre,$"BIENVENIDO/A {nombre}",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
     }
 }
