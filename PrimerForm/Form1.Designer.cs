528e401a418ab33b3a33c92e8fb98eccfb8e02da
Ana Galvez <anamariagalvez01@gmail.com>
2025-03-26 01:18:03 -0300

    Agregar archivos de proyecto.

---
+++ PrimerForm/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PrimerForm
+{
+    partial class frmPrimerProyecto
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNombre = new Label();
+            txtNombre = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNombre.Location = new Point(260, 135);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(109, 31);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Font = new Font("Microsoft Sans Serif", 8.25F);
+            txtNombre.Location = new Point(433, 139);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(266, 20);
+            txtNombre.TabIndex = 1;
+            // 
+            // frmPrimerProyecto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.ForestGreen;
+            ClientSize = new Size(1414, 609);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Name = "frmPrimerProyecto";
+            Text = "Primer Proyecto";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNombre;
+        private TextBox txtNombre;
+    }
+}
---
+++ PrimerForm/Form1.cs
@@ -0,0 +1,15 @@
+namespace PrimerForm
+{
+    public partial class frmPrimerProyecto : Form
+    {
+        public frmPrimerProyecto()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
